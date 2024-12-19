975b54e2dfcef838b78b47682b60cc16ae9de134
davidmtg <montenegro.d@escolar.ifrn.edu.br>
2024-12-19 13:17:36 -0300

    update

---
+++ PowerPlay.com.br/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja de Materiais Esportivos</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stlesheet" href="/css/index.css">
+
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">Bem-vindo à Loja de Materiais Esportivos</h1>
+        <p class="text-center">Confira nossos produtos e faça seus pedidos!</p>
+        <div class="text-center mt-4">
+            <a href="produtos.html" class="btn btn-primary">Ver Produtos</a>
+        </div>
+    </div>
+</body>
+</html>
